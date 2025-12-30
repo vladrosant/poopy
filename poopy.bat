e56c32d76578d824c712daa00f8117e557b4a9ca
vladrosant <pedromuch1@gmail.com>
2025-12-30 09:18:05 -0300

    add initial files

---
+++ poopy.bat
@@ -0,0 +1,2 @@
+@echo off
+python "D:\repos\poopy\poopy.py" %*
---
+++ poopy.go
@@ -0,0 +1,11 @@
+package poopy
+
+data_dir := Path.home() / '.poopy'
+data_file := data_dir / 'expenses.json'
+
+func init_data_file() {
+	data_dir.mkdir(exist_ok=True)
+	if !data_file.exists() {
+
+	}
+}
